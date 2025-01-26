20266cedbc3b9410c1bb88ba73a46d8cc54f3c91
Uri <oriol.segarrapol@gmail.com>
2025-01-26 22:53:02 +0100

    final?

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "695e6493637d5bf2"; }
+/******/ 		__webpack_require__.h = function() { return "9c8a03918c6c8290"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
